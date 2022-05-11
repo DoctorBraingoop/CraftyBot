e8416b64622eb6bb1cb7e7e4c1d6102bae5bd9a1
Saavv <skyviewsmith@gmail.com>
2022-05-11 14:42:34 -0400

    May 11, 2022 2:42 PM

---
+++ index.js
@@ -0,0 +1,8 @@
+// require the necessary discord.js classes
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json")
+
+// create a new client instance
+const bot = new Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"]
+})
