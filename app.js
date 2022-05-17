ca13e7179161413544eb6b7bd1fa0cfb5abab7c8
Saavv <skyviewsmith@gmail.com>
2022-05-16 22:18:06 -0400

    May 16, 2022 10:18 PM

---
+++ app.js
@@ -0,0 +1,57 @@
+// require the necessary discord.js classes
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config.json");
+const fs = require("fs");
+const path = require("node:path");
+
+const $prefix = "$";
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+// ! create a new client instance
+const bot = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
+});
+bot.commands = new Collection();
+
+// ! events
+bot.on("ready", () => {
+  console.log(`
+${bot.user.id} logged in
+Node version: ${process.versions.node}
+  `);
+  console.log("Command files:");
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    console.log(filePath);
+    bot.commands.set(command.name, command);
+  }
+});
+bot.on("guildMemberAdd", async (member) => {
+  const channel = client.channel.cache.get("964967586813059132");
+  if (!channel) return;
+  const embed = new Discord.MessageEmbed().setTitle(`Member Joined`);
+  channel.send(embed);
+});
+bot.on("messageCreate", (msg) => {
+  if (!msg.content.startsWith($prefix) || msg.author.bot) return;
+  const args = msg.content.slice($prefix.length).split(/ +/);
+  if (!args[0]) return;
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case "ping":
+      bot.commands.get("ping").execute(msg,args);
+  }
+
+  console.log(args);
+});
+
+bot.login(token);
+
+//mdn String.slice
+//mdn Array.shift
